597298544fae16b15dad2f4f479392defcd35009
ousllc <info@ous.llc>
2025-01-14 17:17:49 +0900

    fix dist path

--- vite.demo.config.ts
+++ vite.demo.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite';
+import * as path from 'node:path';
 
 export default defineConfig({
+  base: './',
   build: {
     outDir: 'dist/demo',
     rollupOptions: {
       input: {
-        main: './index.html',
-        simple: './examples/simple-form.html',
-        step: './examples/step-form.html'
+        main: path.resolve(__dirname, 'index.html'),
+        simple: path.resolve(__dirname, 'examples/simple-form.html'),
+        step: path.resolve(__dirname, 'examples/step-form.html')
+      },
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]'
       }
     }
   }
